9d22f151d2ebc65e1b2b060137a88b8b11854cd6
Jack Michaud <jack@lomz.me>
2021-05-02 21:06:16 -0700

    Move iosevka build config to non-toml format

--- roles/standard/iosevka.nix
+++ roles/standard/iosevka.nix
@@ -3,13 +3,11 @@ let
   iosevka-fixed = pkgs.iosevka.override {
     set = "fixed";
     # private-build-plans.toml: Generated from https://typeof.net/Iosevka/customizer
-    privateBuildPlan = ''
-      [buildPlans.iosevka-fixed]
-      family = "Iosevka Fixed"
-      spacing = "fixed"
-      serifs = "sans"
-      no-cv-ss = true
-    '';
+    privateBuildPlan = {
+      family = "Iosevka Fixed";
+      spacing = "fixed";
+      serifs = "sans";
+    };
   };
 in
   iosevka-fixed
