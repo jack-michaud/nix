20d17140eefe6a56f2a248087c706e9f108308a8
Jack Michaud <jack@lomz.me>
2021-05-04 22:36:08 -0700

    Use networkmanager for wifi

--- roles/standard/default.nix
+++ roles/standard/default.nix
@@ -14,7 +14,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jack = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" ];
     initialPassword = "jack";
     openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAHXhwJc/cmSfds8ZZmFPVZC7j7mEV8fqnU+IKqAo5fqEDZSOZ2Th949rn3B3+iaZ3yxNog2151Yp8h5ivez3PmFOAGon3qjC9gcWcmxzjcytkZHdJaxOmpHmiuJj44UFq87tETmj+V8+hbyTGvtqTuB5aPhYmVCfApOnQSfbBY1uAnRgA== jack@CASTOR"
@@ -30,6 +30,8 @@
     discord
     git
     ranger
+    vifm
+    z-lua
   ];
 
   nix = {
--- roles/wireless/default.nix
+++ roles/wireless/default.nix
@@ -1,9 +1,9 @@
 { lib, config, pkgs, options, ... }:
 {
 
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.wireless.userControlled.enable = true;
-  networking.wireless.networks.TheFamilyHome_5G.pskRaw = "71cbf415156681914b4db0927f81bb83fc4e1816c312ca79d792607316b09057";
+  networking.networkmanager.enable = true;
+  services.gnome3.gnome-keyring.enable = true;
 
+  users.users.jack.extraGroups = [ "networkmanager" ];
 }
 
