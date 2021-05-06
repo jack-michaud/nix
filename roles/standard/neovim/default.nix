bb295810c2d85edcc6cb68a3b15ad36c777d8c1c
Jack Michaud <jack@lomz.me>
2021-05-05 21:10:08 -0700

    add iosevka to alacritty

--- roles/standard/neovim/default.nix
+++ roles/standard/neovim/default.nix
@@ -10,7 +10,6 @@
     withNodeJs = true;
     withPython3 = true;
   };
-  home-manager.users.jack.home.sessionVariables.EDITOR = "nvim";
   home-manager.users.jack.programs.neovim.extraConfig = ''
     let mapleader = "`"
     map <Space> <leader>
