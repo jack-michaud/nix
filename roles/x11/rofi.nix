b7e682cca0bca2a230d0035c17e858e7f27ec811
Jack Michaud <jack@lomz.me>
2021-05-05 21:41:59 -0700

    feat(rofi): Add drun and emoji modi

--- roles/x11/rofi.nix
+++ roles/x11/rofi.nix
@@ -1,6 +1,17 @@
-{ home-manager, ... }:
+{ home-manager, pkgs, ... }:
 {
+  
   home-manager.users.jack.programs.rofi = {
     enable = true;
+    # Theming was done in xresources
+    extraConfig = {
+      modi = "drun,emoji";
+      kb-primary-paste = "Alt+V";
+      kb-primary-copy = "Alt+C";
+    };
+
+    package = pkgs.rofi.override {
+      plugins = [ pkgs.rofi-emoji ];
+    };
   };
 }
