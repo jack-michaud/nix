3a8dc6ae9b9dcc4661948cfd0529b3866115f1ad
Jack Michaud <jack@lomz.me>
2021-05-22 09:35:18 -0700

    feat(darwin): add some darwin ricing configs

--- roles/standard/darwin.nix
+++ roles/standard/darwin.nix
@@ -1,4 +1,5 @@
 { lib, config, pkgs, options, ... }:
+{ lib, config, pkgs, options, username, ... }:
 { 
   imports = [
     ./common.nix
@@ -6,4 +7,25 @@
   ];
 
   environment.shells = [ pkgs.zsh "/Users/jack/.nix-profile/bin/fish" ];
+  system.defaults = {
+    dock = {
+      show-recents = false;
+      autohide = true;
+    };
+    finder = {
+      # show desktop icons
+      CreateDesktop = false;
+      # warn on changing file extension
+      FXEnableExtensionChangeWarning = false;
+      # show file extensions
+      AppleShowAllExtensions = true;
+    };
+    NSGlobalDomain = {
+      # dark mode
+      AppleInterfaceStyle = "Dark";
+      # hide menu bar
+      _HIHideMenuBar = false;
+    };
+  };
+
 }
