f18220e0c295942fb47412325151424937e8ce7e
Jack Michaud <jack@lomz.me>
2021-05-25 08:13:32 -0700

    feat: add default file that all configurations import from

---
+++ default.nix
@@ -0,0 +1,71 @@
+{ inputs, config, lib, pkgs, isDarwin, ... }:
+
+with lib;
+with lib.my;
+{
+  imports =
+    # I use home-manager to deploy files to $HOME; little else
+    [ (if isDarwin then inputs.home-manager.darwinModules.home-manager else inputs.home-manager.nixosModules.home-manager) ]
+    # All my personal modules
+    ++ (mapModulesRec' (toString ./modules) import);
+
+  # Common config for all nixos machines; and to ensure the flake operates
+  # soundly
+  environment.variables.DOTFILES = config.dotfiles.dir;
+  environment.variables.DOTFILES_BIN = config.dotfiles.binDir;
+
+  # Configure nix and nixpkgs
+  environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
+  nix =
+    let filteredInputs = filterAttrs (n: _: n != "self") inputs;
+        nixPathInputs  = mapAttrsToList (n: v: "${n}=${v}") filteredInputs;
+        registryInputs = mapAttrs (_: v: { flake = v; }) filteredInputs;
+    in {
+      package = pkgs.nixFlakes;
+      extraOptions = "experimental-features = nix-command flakes";
+      nixPath = nixPathInputs ++ [
+        "nixpkgs-overlays=${config.dotfiles.dir}/overlays"
+        "dotfiles=${config.dotfiles.dir}"
+      ];
+      binaryCaches = [
+        "https://nix-community.cachix.org"
+        "https://jack-michaud-ajax.cachix.org"
+      ];
+      binaryCachePublicKeys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "jack-michaud-ajax.cachix.org-1:/AsBHtSL31exwTiTTnyPBiDA00HE+BkBAnl7NiwRQpw="
+      ];
+      registry = registryInputs // { dotfiles.flake = inputs.self; };
+      autoOptimiseStore = true;
+    };
+  system.configurationRevision = with inputs; mkIf (self ? rev) self.rev;
+  system.stateVersion = "21.05";
+
+
+  ## Some reasonable, global defaults
+  # This is here to appease 'nix flake check' for generic hosts with no
+  # hardware-configuration.nix or fileSystem config.
+  fileSystems."/".device = mkDefault "/dev/disk/by-label/nixos";
+
+  # Use the latest kernel
+  boot = {
+    kernelPackages = mkDefault pkgs.linuxPackages_5_10;
+    loader = {
+      efi.canTouchEfiVariables = mkDefault true;
+      systemd-boot.configurationLimit = 10;
+      systemd-boot.enable = mkDefault true;
+    };
+  };
+
+  # Just the bear necessities...
+  environment.systemPackages = with pkgs; [
+    bind
+    cached-nix-shell
+    coreutils
+    git
+    vim
+    wget
+    gnumake
+    unzip
+  ];
+}
