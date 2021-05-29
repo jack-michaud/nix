9e19fbeebdd289024297c165c09642c1e659005f
Jack Michaud <jack@lomz.me>
2021-05-28 23:49:13 -0700

    refactor(nvim): Move init.vim into ../config/nvim

---
+++ config/nvim/init.vim
@@ -0,0 +1,59 @@
+let mapleader = "`"
+map <Space> <leader>
+
+inoremap jk <Esc>
+
+" Copy to clipboard
+vnoremap  <leader>y  "+y
+nnoremap  <leader>Y  "+yg_
+nnoremap  <leader>y  "+y
+nnoremap  <leader>yy  "+yy
+
+" Paste from clipboard
+nnoremap <leader>p "+p
+nnoremap <leader>P "+P
+vnoremap <leader>p "+p
+vnoremap <leader>P "+P
+
+" File Browser
+let g:netrw_liststyle = 3
+let g:netrw_banner = 0
+let g:netrw_browse_split = 4
+let g:netrw_winsize = 20
+let g:netrw_altv = 20
+noremap <leader>r :Vexplore<Enter>
+
+" Fuzzy find
+nnoremap <leader>F :Files<enter>
+nnoremap <leader>G :Rg<enter>
+nnoremap <leader>b :Buffers<enter>
+nnoremap <leader>S :cex system("rg --column ")<Left><Left>
+
+" Opens Git link for selected line or region in browser
+noremap <leader>gb  :Gbrowse<Enter>
+" Adding hunks with :Gstatus - https://vi.stackexchange.com/a/21410
+"  - Press "=" on an file (shows git diff)
+"  - Press "-" on a hunk or visual selection to stage/unstage
+"  - "cvc" to commit verbosely
+noremap <leader>gs  :Git<Enter>
+noremap <leader>gl  :Glog<Enter>
+
+"" Coc-snippets
+" Use <C-l> for trigger snippet expand.
+inoremap <C-l> <Plug>(coc-snippets-expand)
+
+" Use <C-j> for select text for visual placeholder of snippet.
+vnoremap <C-j> <Plug>(coc-snippets-select)
+
+" Use <C-j> for jump to next placeholder, it's default of coc.nvim
+let g:coc_snippet_next = '<c-j>'
+
+" Use <C-k> for jump to previous placeholder, it's default of coc.nvim
+let g:coc_snippet_prev = '<c-k>'
+
+" Use <C-j> for both expand and jump (make expand higher priority.)
+inoremap <C-j> <Plug>(coc-snippets-expand-jump)
+
+" Use <leader>x for convert visual selected code to snippet
+xnoremap <leader>x  <Plug>(coc-convert-snippet)
+
--- modules/shared/editors/nvim/default.nix
+++ modules/shared/editors/nvim/default.nix
@@ -17,47 +17,7 @@ in {
       vimAlias = true;
       withNodeJs = true;
       withPython3 = true;
-      extraConfig = ''
-        let mapleader = "`"
-        map <Space> <leader>
-
-        inoremap jk <Esc>
-
-        " Copy to clipboard
-        vnoremap  <leader>y  "+y
-        nnoremap  <leader>Y  "+yg_
-        nnoremap  <leader>y  "+y
-        nnoremap  <leader>yy  "+yy
-
-        " Paste from clipboard
-        nnoremap <leader>p "+p
-        nnoremap <leader>P "+P
-        vnoremap <leader>p "+p
-        vnoremap <leader>P "+P
-
-        " File Browser
-        let g:netrw_liststyle = 3
-        let g:netrw_banner = 0
-        let g:netrw_browse_split = 4
-        let g:netrw_winsize = 20
-        let g:netrw_altv = 20
-        noremap <leader>r :Vexplore<Enter>
-
-        " Fuzzy find
-        nnoremap <leader>F :Files<enter>
-        nnoremap <leader>G :Rg<enter>
-        nnoremap <leader>b :Buffers<enter>
-        nnoremap <leader>S :cex system("rg --column ")<Left><Left>
-
-        " Opens Git link for selected line or region in browser
-        noremap <leader>gb  :Gbrowse<Enter>
-        " Adding hunks with :Gstatus - https://vi.stackexchange.com/a/21410
-        "  - Press "=" on an file (shows git diff)
-        "  - Press "-" on a hunk or visual selection to stage/unstage
-        "  - "cvc" to commit verbosely
-        noremap <leader>gs  :Git<Enter>
-        noremap <leader>gl  :Glog<Enter>
-      '';
+      extraConfig = (builtins.readFile "${config.dotfiles.configDir}/nvim/init.vim");
       plugins = with pkgs.vimPlugins; [
         vim-fugitive
         vim-rhubarb
@@ -66,6 +26,7 @@ in {
         vim-nix
         fzf-vim
         coc-nvim
+        coc-snippets
       ];
     };
   };
