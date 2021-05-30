4d94d9ba4957d8f65c3ca0fc0cd4d082004ec302
Jack Michaud <jack@lomz.me>
2021-05-30 15:48:35 -0700

    feat(modules): syncthing shared module

--- modules/linux/services/syncthing.nix
+++ modules/linux/services/syncthing.nix
@@ -5,9 +5,6 @@ with lib.my;
 let cfg = config.modules.services.syncthing;
     username = config.user.name;
 in {
-  options.modules.services.syncthing = {
-    enable = mkBoolOpt false;
-  };
   config = mkIf cfg.enable {
     services.syncthing = {
       enable = true;
