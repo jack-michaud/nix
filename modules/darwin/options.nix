a65d135efc566aedf00f55f86a0906176376988f
Jack Michaud <jack@lomz.me>
2021-05-30 15:59:41 -0700

    feat(modules): add some darwin defaults

--- modules/darwin/options.nix
+++ modules/darwin/options.nix
@@ -7,5 +7,28 @@ with lib.my;
   options = with types; {
   };
   config = {
+    environment.systemPath = [ "/usr/local/bin/" ];
+
+    system.defaults = {
+      dock = {
+        show-recents = false;
+        autohide = true;
+      };
+      finder = {
+        # show desktop icons
+        CreateDesktop = false;
+        # warn on changing file extension
+        FXEnableExtensionChangeWarning = false;
+        # show file extensions
+        AppleShowAllExtensions = true;
+      };
+      NSGlobalDomain = {
+        # dark mode
+        AppleInterfaceStyle = "Dark";
+        # hide menu bar
+        _HIHideMenuBar = false;
+      };
+    };
+
   };
 }
