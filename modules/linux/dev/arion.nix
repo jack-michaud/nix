3a14d15130be0771c5570a8ac888be7dc268ba1f
Jack Michaud <jack@lomz.me>
2021-06-22 00:11:14 -0700

    feat(modules): arion

---
+++ modules/linux/dev/arion.nix
@@ -0,0 +1,31 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.arion;
+in {
+  options.modules.dev.arion = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    # https://docs.hercules-ci.com/arion/
+    environment.systemPackages = [
+      pkgs.arion
+      # Do install the docker CLI to talk to podman.
+      # Not needed when virtualisation.docker.enable = true;
+      pkgs.docker-client
+    ];
+
+    # Arion works with Docker, but for NixOS-based containers, you need Podman
+    # since NixOS 21.05.
+    virtualisation.docker.enable = false;
+    virtualisation.podman.enable = true;
+    virtualisation.podman.dockerSocket.enable = true;
+    virtualisation.podman.defaultNetwork.dnsname.enable = true;
+
+    # Use your username instead of `myuser`
+    user.extraGroups = ["podman"];
+  };
+}
+
