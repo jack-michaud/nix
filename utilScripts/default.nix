5c7fd799ef24dac4f43088df6fd237ca25d680ef
Jack Michaud <jack@lomz.me>
2021-09-18 17:07:56 -0700

    feat(utilScripts): aws assume role script

--- utilScripts/default.nix
+++ utilScripts/default.nix
@@ -34,4 +34,20 @@ rec {
   use-vpn = callPackage ./use-vpn.nix {
     inherit rofi-ask-pass;
   };
+
+  assumeRole = let
+    awsBin = "${awscli2}/bin/aws";
+    jqBin = "${jq}/bin/jq";
+  in
+    writeShellScriptBin "assumeRole" ''
+      role=$(${awsBin} sts assume-role --role-arn $1 --role-session-name dev-assume)
+      
+      
+      echo "# Evaluate me! Assumed role $(echo $role | ${jqBin} '.AssumedRoleUser.Arn')"
+      cat <<EOF
+      export AWS_ACCESS_KEY_ID=$(echo $role | ${jqBin} -r '.Credentials.AccessKeyId')
+      export AWS_SECRET_ACCESS_KEY=$(echo $role | ${jqBin} -r '.Credentials.SecretAccessKey')
+      export AWS_SESSION_TOKEN=$(echo $role | ${jqBin} -r '.Credentials.SessionToken')
+      EOF
+    '';
 }
