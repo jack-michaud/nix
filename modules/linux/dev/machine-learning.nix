a34e6e63608205d5e098232efce366e936c3e540
Jack Michaud <jack@lomz.me>
2021-12-06 21:33:07 -0800

    feat: add machine learning module

---
+++ modules/linux/dev/machine-learning.nix
@@ -0,0 +1,17 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.machine-learning;
+in {
+  options.modules.dev.machine-learning = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      python39Packages.pyopengl
+    ];
+  };
+}
+
