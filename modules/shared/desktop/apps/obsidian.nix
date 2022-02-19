4d632fa30a22c2e004b7c6d2b249560b3a74b9fb
Jack Michaud <jack@lomz.me>
2022-02-18 18:36:23 -0800

    fix(obsidian): disable custom css

--- modules/shared/desktop/apps/obsidian.nix
+++ modules/shared/desktop/apps/obsidian.nix
@@ -4,18 +4,15 @@ with lib;
 with lib.my;
 
 let cfg = config.modules.desktop.apps.obsidian;
-in { 
-  options.modules.desktop.apps.obsidian = {
-    enable = mkBoolOpt false;
-  };
+in {
+  options.modules.desktop.apps.obsidian = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    home.configFile = let 
-      obsidian-theme = generators.toCSSFile ../../../../config/obsidian-theme/default.scss;
-    in {
-      "${config.dotfiles.vaultDir}/personal/.obsidian/themes/theme.css".source = obsidian-theme;
-      "${config.dotfiles.vaultDir}/private/.obsidian/themes/theme.css".source = obsidian-theme;
-    };
+    #home.configFile = let 
+    #  obsidian-theme = generators.toCSSFile ../../../../config/obsidian-theme/default.scss;
+    #in {
+    #  "${config.dotfiles.vaultDir}/personal/.obsidian/themes/theme.css".source = obsidian-theme;
+    #  "${config.dotfiles.vaultDir}/private/.obsidian/themes/theme.css".source = obsidian-theme;
+    #};
   };
 }
-
