d8e7567840c7db122932b2fb4076f037951190ba
Jack Michaud <jack@lomz.me>
2022-02-18 18:35:23 -0800

    fix(dwm): add gnome keyring

--- modules/linux/wms/dwm.nix
+++ modules/linux/wms/dwm.nix
@@ -4,9 +4,7 @@ with lib;
 with lib.my;
 let cfg = config.modules.wms.dwm;
 in {
-  options.modules.wms.dwm = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.wms.dwm = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     #assertions = [
@@ -17,9 +15,12 @@ in {
     #];
     # Enable the X11 windowing system.
     services.xserver.enable = true;
+    security.pam.services.lightdm.enableGnomeKeyring = true;
     services.xserver.windowManager.dwm.enable = true;
     services.xserver.displayManager.sessionCommands = ''
-      ${pkgs.feh}/bin/feh --bg-scale --no-xinerama ${../themes/dark-blue/wallpaper/image.png}
+      ${pkgs.feh}/bin/feh --bg-scale --no-xinerama ${
+        ../themes/dark-blue/wallpaper/image.png
+      }
       while true; do
         ${pkgs.dwm}/bin/dwmStatus
         sleep 3 &
@@ -53,5 +54,6 @@ in {
 
     # Enable touchpad support (enabled default in most desktopManager).
     services.xserver.libinput.enable = true;
+
   };
 }
