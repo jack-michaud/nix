c0ad8568b3a556ed5feb50622ca5421f0fc15edd
Jack Michaud <jack@lomz.me>
2022-11-06 08:53:35 -0500

    feat(cloner): update cloner

--- packages/cloner.nix
+++ packages/cloner.nix
@@ -1,16 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 with pkgs;
 let
   src = fetchFromGitHub {
     owner = "jack-michaud";
     repo = "cloner";
     rev = "main";
-    sha256 = "1cwwivdj2bahdcxmzczlk80hq6mmwz1r4260y2k1vkgjpf2qydvx";
+    sha256 = "0rckskppv9i47q1nr56jhdhf11ia0r1zl9mc3vp0biwwn5h0kcyi";
   };
-in 
-  rustPlatform.buildRustPackage {
-    inherit src;
-    name = "cloner";
-    version = "0.0.1";
-    cargoHash = "sha256-dPkeDaITJlDuzdih4Ax8mk/Pv1KKsOMkT6Kf7KXuB3w=";
-  }
+in rustPlatform.buildRustPackage {
+  inherit src;
+  name = "cloner";
+  version = "0.0.1";
+  cargoHash = "sha256-dPkeDaITJlDuzdih4Ax8mk/Pv1KKsOMkT6Kf7KXuB3w=";
+}
