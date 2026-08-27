0e18ba03ba4b91428054511f41ee82ca595192d5
Jack Michaud <jack@lomz.me>
2026-08-27 10:58:09 -0400

    feat(nvim): add NVIM_LAZY_GIT_PROTOCOL env var to control http/ssh cloning, default ssh

--- modules/shared/editors/nvim/config/lua/config/lazy.lua
+++ modules/shared/editors/nvim/config/lua/config/lazy.lua
@@ -1,7 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  print(vim.fn.system({"which", "git"}))
+
+-- Read protocol from environment, default to ssh
+local git_protocol = os.getenv("NVIM_LAZY_GIT_PROTOCOL") or "ssh"
+if git_protocol ~= "ssh" and git_protocol ~= "http" and git_protocol ~= "https" then
+  git_protocol = "ssh"
+end
+
+-- Optional disable clone flag
+local disable_clone = os.getenv("NVIM_LAZY_DISABLE_CLONE") == "1" or os.getenv("NVIM_LAZY_DISABLE_CLONE") == "true"
+
+local function git_url(user, repo)
+  if git_protocol == "http" or git_protocol == "https" then
+    return string.format("https://github.com/%s/%s.git", user, repo)
+  else
+    return string.format("git@github.com:%s/%s.git", user, repo)
+  end
+end
+
+if not disable_clone and not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = git_url("folke", "lazy.nvim")
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
@@ -16,44 +33,35 @@ end
 
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+local git_url_format
+if git_protocol == "http" or git_protocol == "https" then
+  git_url_format = "https://github.com/%s.git"
+else
+  git_url_format = "git@github.com:%s"
+end
+
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    --{ import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    --{ import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- { import = "lazyvim.plugins.extras.ai.copilot" },
-    -- { import = "lazyvim.plugins.extras.ui.edgy" },
-    -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins.formatting" },
     { import = "plugins.linting" },
     { import = "plugins.lsp" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
@@ -62,7 +70,6 @@ require("lazy").setup({
     },
   },
   git = {
-    -- Use ssh protocol for git clones
-    url_format = "git@github.com:%s",
+    url_format = git_url_format,
   },
 })
